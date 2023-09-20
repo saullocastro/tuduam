f8ab7c76470cc2fd681b2c90733df53cdc414b2f
Saullo G. P. Castro <saullogiovani@gmail.com>
2023-09-20 15:50:16 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+TU Delft, Urban Air Mobility
+---
+
+Github Actions status:
+
+
+Coverage status:
+
+
+
+TU Delft
+
+Urban Air Mobility
+
+
+This Python module can be used by the students to assist in the course assignments.
+
+
+
+
+License
+-------
+Distrubuted under the 3-Clause BSD license
+(https://raw.github.com/saullocastro/tudaesasII/master/LICENSE).
+
+Contact: S.G.P.Castro@tudelft.nl
+
---
+++ setup.py
@@ -0,0 +1,112 @@
+import os
+import inspect
+import subprocess
+from setuptools import setup, find_packages
+
+
+is_released = False
+version = '2023.0'
+
+
+def git_version():
+    def _minimal_ext_cmd(cmd):
+        # construct minimal environment
+        env = {}
+        for k in ['SYSTEMROOT', 'PATH']:
+            v = os.environ.get(k)
+            if v is not None:
+                env[k] = v
+        # LANGUAGE is used on win32
+        env['LANGUAGE'] = 'C'
+        env['LANG'] = 'C'
+        env['LC_ALL'] = 'C'
+        out = subprocess.Popen(cmd, stdout=subprocess.PIPE, env=env).communicate()[0]
+        return out
+
+    try:
+        out = _minimal_ext_cmd(['git', 'rev-parse', 'HEAD'])
+        git_revision = out.strip().decode('ascii')
+    except OSError:
+        git_revision = "Unknown"
+
+    return git_revision
+
+
+def get_version_info(version, is_released):
+    fullversion = version
+    if not is_released:
+        git_revision = git_version()
+        fullversion += '.dev0+' + git_revision[:7]
+    return fullversion
+
+
+def write_version_py(version, is_released, filename='tuduam/version.py'):
+    fullversion = get_version_info(version, is_released)
+    with open("./tuduam/version.py", "wb") as f:
+        f.write(b'__version__ = "%s"\n' % fullversion.encode())
+    return fullversion
+
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    setupdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+    return open(os.path.join(setupdir, fname)).read()
+
+
+#_____________________________________________________________________________
+
+install_requires = [
+        "numpy",
+        "scipy",
+        ]
+
+#Trove classifiers
+CLASSIFIERS = """\
+
+Development Status :: 1 - Planning
+Intended Audience :: Education
+Intended Audience :: Science/Research
+Intended Audience :: Developers
+Topic :: Scientific/Engineering
+Topic :: Scientific/Engineering :: Mathematics
+Topic :: Education
+Topic :: Software Development
+Topic :: Software Development :: Libraries :: Python Modules
+Operating System :: Microsoft :: Windows
+Operating System :: Unix
+Operating System :: POSIX :: BSD
+Programming Language :: Python :: 3.8
+Programming Language :: Python :: 3.9
+Programming Language :: Python :: 3.10
+Programming Language :: Python :: 3.11
+License :: OSI Approved :: BSD License
+
+"""
+
+fullversion = write_version_py(version, is_released)
+
+data_files = [('', [
+        'README.md',
+        'LICENSE',
+        ])]
+
+s = setup(
+    name = "tuduam",
+    version = fullversion,
+    author = "Saullo G. P. Castro",
+    author_email = "S.G.P.Castro@tudelft.nl",
+    description = ("Python module related to the course on Urban Air Mobility at the faculty of Aeropsace Engineering, TU Delft"),
+    license = "3-Clause BSD",
+    keywords = "eVTOL design",
+    url = "https://github.com/saullocastro/tuduam",
+    packages=find_packages(),
+    data_files=data_files,
+    long_description=read('README.md'),
+    classifiers=[_f for _f in CLASSIFIERS.split('\n') if _f],
+    install_requires=install_requires,
+    include_package_data=True,
+)
+
