64b3d68cad8ed7d7ff2a3914a78f84e2bac6ef56
Damien Keijzer <damien.keijzer@gmail.com>
2023-12-30 17:56:21 +0100

    BUG: Insert missing line notebook and fix __init__
    A commented line required in the notebook was missing and an import
    statment in the __init__.py was causing the test to fail on Github.

--- tuduam/__init__.py
+++ tuduam/__init__.py
@@ -1,10 +1,5 @@
-import sys
-import os
-
-sys.path.append(os.path.dirname(__file__))
 
 from data_structures import *
 from aerodynamics import lift_curve_slope, planform_geometry
-from legacy.legacy_structures import wingbox_optimization
 
 # __all__ = ["aerdynamics", "data_structures", "structures"]
